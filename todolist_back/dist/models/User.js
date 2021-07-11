baf146eef5ab78336b3cd313965a56281eae12f7
tsandovalr <63921337+tsandovalr@users.noreply.github.com>
2021-07-10 21:51:15 -0400

    Add files via upload

---
+++ todolist_back/dist/models/User.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = require("mongoose");
+const userSchema = new mongoose_1.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+        maxlength: 64
+    }
+});
+exports.default = mongoose_1.model('User', userSchema);
---
+++ todolist_back/src/models/User.ts
@@ -0,0 +1,23 @@
+import { Schema, model } from 'mongoose';
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+        maxlength: 64
+    }
+});
+export default model('User', userSchema);
